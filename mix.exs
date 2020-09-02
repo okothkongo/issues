12df8bc6560b0c03300852fcc8a57c3e77c71e10
okothkongo <okothkongo@gmail.com>
2020-09-02 05:26:50 +0300

    doc test and test coverage

--- mix.exs
+++ mix.exs
@@ -10,6 +10,13 @@ defmodule Issues.MixProject do
       version: "0.1.0",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        "coveralls": :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ],
       deps: deps()
     ]
   end
@@ -27,7 +34,8 @@ defmodule Issues.MixProject do
       {:httpoison, "~> 1.7.0"},
       {:poison, "~> 4.0"},
       {:ex_doc, "~> 0.22.2"},
-      {:earmark, "~> 1.4"}
+      {:earmark, "~> 1.4"},
+      {:excoveralls, "~> 0.13.1"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
